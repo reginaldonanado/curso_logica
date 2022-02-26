da1191ce34d14cbbc657ce6b36350b9f163904f3
Reginaldo Lopes <reginaldonanado@gmail.com>
2022-02-26 17:44:19 -0300

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome1, nome2;
+		double salario1, salario2;
+		int idade;
+		char sexo;
+		
+		System.out.println("Digite o nnome da primeira pessoa: ");
+		nome1 = sc .nextLine();
+		System.out.println("Digite o salario da primeira pessoa: ");
+		salario1 = sc.nextDouble();
+		
+		System.out.println("Digite o nnome da segunda pessoa: ");
+		sc.nextLine();
+		nome2 = sc .nextLine();
+		System.out.println("Digite o salario da segunada pessoa: ");
+		salario2 = sc.nextDouble();
+		
+		System.out.println("Digite uma idade: ");
+		idade = sc.nextInt();
+		System.out.println("Digite um sexo (F/M): ");
+		sexo = sc.next().charAt(0);
+		
+		System.out.println("Nome 1: " + nome1); 
+		System.out.println("Salario 1: " + String.format("%.2f", salario1)); 
+		System.out.println("Nome 2: " + nome2); 
+	    System.out.println("Salario 2: " + String.format("%.2f", salario2)); 
+		System.out.println("Idade: " + idade); 
+		System.out.println("Sexo: " + sexo); 
+		
+		sc.close();
+
+		
+	}
+
+}
